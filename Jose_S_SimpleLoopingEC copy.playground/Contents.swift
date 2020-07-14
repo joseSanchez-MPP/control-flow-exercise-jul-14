8fb9ad1f7e358aac11293873994b7f030032df89
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-14 15:41:13 -0500

    minor changes

--- Jose_S_SimpleLoopingEC copy.playground/Contents.swift
+++ Jose_S_SimpleLoopingEC copy.playground/Contents.swift
@@ -1,9 +1,12 @@
 import UIKit
-let maxMarches = 10 // counts 10 as 9 for the purposes of the following array; index starts at 0
-let numbersInWords = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"] //this could also be done with a bunch of switches, but to me this seems the simplest.
-let customPhrases = [ "suck his thumb", "tie his shoe", "climb a tree", "shut the door", "take a dive", "pick up sticks", "pray to heaven", "roller skate", "check the time", "shout \"The End!\""]
 
-for currentMarch in 0...maxMarches - 1 { //also counts from 0 due to the array
+//I'm trying to make this to use as least lines as possible.
+
+let maxMarches = 10 // counts 10 as 9 for the purposes of the following array; index starts at 0
+let numbersInWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"] //this could also be done with a bunch of switches, but to me this seems the simplest.
+let customPhrases = ["suck his thumb", "tie his shoe", "climb a tree", "shut the door", "take a dive", "pick up sticks", "pray to heaven", "roller skate", "check the time", "shout \"The End!\""]
+
+for currentMarch in 0...maxMarches - 1 {
     for a in 1...3 {
         print("The ants go marching \(numbersInWords[currentMarch]) by \(numbersInWords[currentMarch])" + (a != 3 ? ", hurrah, hurrah" : ""))
     }
