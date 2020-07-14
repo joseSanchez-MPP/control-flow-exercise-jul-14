2541b40315003a84abbd3575ec80a321e36f6ee4
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-14 13:55:42 -0500

    Made it a bit better

--- Jose_S_ControlFlowExercise.playground/Contents.swift
+++ Jose_S_ControlFlowExercise.playground/Contents.swift
@@ -2,13 +2,18 @@ import UIKit
 
 let timeoutInSeconds = 180
 var currentTime = 0
+var timeWord = "second"
 
-print("Timer's set for \(timeoutInSeconds) seconds")
+print("Timer's set for \(timeoutInSeconds)")
 print("\n")
 
 for currentTime in 1...timeoutInSeconds {
     sleep(1)
-    print("\(currentTime) seconds have passed!")
+    if (currentTime != 1) {
+        timeWord = "seconds"
+        
+    }
+    print("\(currentTime) \(timeWord) have passed!")
 }
 
 print("\n")
