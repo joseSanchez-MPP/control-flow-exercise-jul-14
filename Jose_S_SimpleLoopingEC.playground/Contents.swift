40516076bd1aa2e17a57939fdfa1c8a581e31b17
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-14 14:40:11 -0500

    Minor edits

--- Jose_S_SimpleLoopingEC.playground/Contents.swift
+++ Jose_S_SimpleLoopingEC.playground/Contents.swift
@@ -9,21 +9,22 @@ for currentMarch in 0...maxMarches { //also counts from 0 due to the array
     print("The ants go marching \(numbersInWords[currentMarch]) by \(numbersInWords[currentMarch])")
     
     switch(currentMarch) {
-    case 0: currentCustomPhrase = "suck his thumb" //I know this could be done in an array, but still want to demonstrate I know how to use switch
-    case 1: currentCustomPhrase = "tie his shoe"
-    case 2: currentCustomPhrase = "climb a tree"
-    case 3: currentCustomPhrase = "shut the door"
-    case 4: currentCustomPhrase = "take a dive"
-    case 5: currentCustomPhrase = "pick up sticks"
-    case 6: currentCustomPhrase = "pray to heaven"
-    case 7: currentCustomPhrase = "roller skate"
-    case 8: currentCustomPhrase = "check the time"
-    case 9: currentCustomPhrase = "shout \"The End\""
-    default: currentCustomPhrase = "- actually, he doesn\'t stop!"
+        case 0: currentCustomPhrase = "suck his thumb" // this could be done in an array, but still want to demonstrate I know how to use switch
+        case 1: currentCustomPhrase = "tie his shoe"
+        case 2: currentCustomPhrase = "climb a tree"
+        case 3: currentCustomPhrase = "shut the door"
+        case 4: currentCustomPhrase = "take a dive"
+        case 5: currentCustomPhrase = "pick up sticks"
+        case 6: currentCustomPhrase = "pray to heaven"
+        case 7: currentCustomPhrase = "roller skate"
+        case 8: currentCustomPhrase = "check the time"
+        case 9: currentCustomPhrase = "shout \"The End\""
+        default: currentCustomPhrase = "- actually, he doesn\'t stop"
 }
-    print("The little one stops to \(currentCustomPhrase)")
+    print("The little one stops to \(currentCustomPhrase),")
     print("And they all go marching down to the ground")
-    print("To get out of the rain\(currentMarch != 9 ? ", BOOM! BOOM! BOOM!\n\n" : ".")") // doesn't include BOOM! if it's the last verse. Uses Swift's Ternary conditional operator. Hopefully it's not too bad to understand.
+    print("To get out of the rain" + (currentMarch != 9 ? ", BOOM! BOOM! BOOM!" : ".")) // doesn't include BOOM! if it's the last verse. Uses Swift's Ternary conditional operator. Hopefully it's not too bad to understand.
+    print("\n")
 }
 
 
